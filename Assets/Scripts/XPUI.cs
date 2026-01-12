8277f99bd5a79b554ae6947fc85d140a01d42264
LIL-DY <151176129+LIL-DY@users.noreply.github.com>
2026-01-12 12:03:52 +0100

    level uppgrade

--- Assets/Scripts/XPUI.cs
+++ Assets/Scripts/XPUI.cs
@@ -7,14 +7,18 @@ public class XPUI : MonoBehaviour
     public PlayerProgression playerprogression;
     public Slider xpBar;
     public TMP_Text levelText;
+    public TMP_Text pointsText;
+
 
     void Update()
     {
-        
+        if (playerprogression == null || xpBar == null || levelText == null || pointsText == null) return;
+
         xpBar.maxValue = playerprogression.xpNeeded;
         xpBar.value = playerprogression.xp;
+        pointsText.text = "Points: " + playerprogression.upgradePoints;
 
-        
+
         levelText.text = "Level: " + playerprogression.level;
     }
 }
