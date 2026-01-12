f5725cb7078051d85dd9de0cddf0a1a42d0649d4
SerhiiVatsyk <serhii.vatsyk@elev.ga.lbs.se>
2026-01-12 12:11:44 +0100

    Enemy mocement och attack

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public int damage = 1;
+    public float attackRange = 1f;
+    public LayerMask enemyLayer;
+
+    public float attackCooldown = 1f; 
+    private float lastAttackTime = 0f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            TryAttack();
+        }
+    }
+
+    void TryAttack()
+    {
+        
+        if (Time.time - lastAttackTime >= attackCooldown)
+        {
+            Attack();
+            lastAttackTime = Time.time; 
+        }
+    }
+
+    void Attack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, attackRange, enemyLayer);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<EnemyHealth>().TakeDamage(damage);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth;
+
+    public TMP_Text healthText;
+
+    private void Start()
+    {
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthText();
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth += amount;
+
+        
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        UpdateHealthText();
+
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void UpdateHealthText()
+    {
+        healthText.text = "HP: " + currentHealth + "/ " + maxHealth;
+    }
+}
