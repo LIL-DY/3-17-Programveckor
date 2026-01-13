c896b09bb6ae3e958a260633474c1c9b30bf3677
TheoLSu23c <154343177+TheoLSu23c@users.noreply.github.com>
2026-01-13 21:39:50 +0100

    update damage (players)

--- Assets/Scripts/Theos scripts/Player Attack.cs
+++ Assets/Scripts/Theos scripts/Player Attack.cs
@@ -1,48 +1,70 @@
+using System.Collections;
 using UnityEngine;
 
-public class PlayerAttack3: MonoBehaviour
+public class PlayerAttackFinal : MonoBehaviour
 {
-    private GameObject attackArea = default;
+    public int damage = 1;
+    public float attackRange = 1f;
+    public LayerMask enemyLayer;
+    public float attackCooldown = 2f;
+    private float lastAttckTime = 0f;
 
+    private GameObject attackArea;
     private bool attacking = false;
-    private float AttackTime = 0.25f;
+    public float attackTime = 0.2f;
     private float timer = 0f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Start()
     {
         attackArea = transform.GetChild(0).gameObject;
         attackArea.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.J) || Input.GetKeyDown(KeyCode.Mouse0))
         {
-
-
-
-            StartAttack();
-
+            TryAttck();
         }
-
         if (attacking)
         {
             timer += Time.deltaTime;
-
-            if(timer >= AttackTime)
+            if(timer >= attackTime)
             {
-                timer = 0;
+                timer = 0f;
                 attacking = false;
-                attackArea.SetActive(attacking);
+                attackArea.SetActive(false);
             }
         }
     }
 
-    private void StartAttack()
+    void TryAttck()
+    {
+        if(Time.time - lastAttckTime >= attackCooldown)
+        {
+            StartAttack();
+            lastAttckTime = Time.time;
+        }
+    }
+
+    void StartAttack()
     {
         attacking = true;
         timer = 0f;
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 Direction = (mousePos - transform.position).normalized;
+        Vector2 AttackPos = transform.position + new Vector3(Direction.x, Direction.y, 0) * attackRange;
+        attackArea.transform.position = AttackPos;
+
+        float angle = Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg;
+        attackArea.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
         attackArea.SetActive(true);
+        Collider2D[] HittEnemies = Physics2D.OverlapBoxAll(attackArea.transform.position, attackArea.GetComponent<Collider2D>().bounds.size, 0f, enemyLayer);
+        foreach(Collider2D enemy in HittEnemies)
+        {
+            enemy.GetComponent<EnemyHealth>()?.TakeDamage(damage);
+        }
     }
 }
