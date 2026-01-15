84ffe35760965e7eb4e5ed7c4818b7c6a2bb861e
LIL-DY <151176129+LIL-DY@users.noreply.github.com>
2026-01-15 11:26:10 +0100

    ATTACK

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -5,39 +5,54 @@ public class PlayerAttack : MonoBehaviour
     public int damage = 1;
     public float attackRange = 1f;
     public LayerMask enemyLayer;
+    public Animator animator;
 
-    public float attackCooldown = 1f; 
-    private float lastAttackTime = 0f;
+    public float attackCooldown = 1f;
+    private float lastAttackTime = -999f;
 
-    private void Update()
+    void Awake()
     {
-        if (Input.GetKeyDown(KeyCode.J))
-        {
+        if (animator == null)
+            animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Välj vilka knappar du vill ha:
+        bool pressed = Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.J);
+
+        if (pressed)
             TryAttack();
-        }
     }
 
     void TryAttack()
     {
-        
-        if (Time.time - lastAttackTime >= attackCooldown)
-        {
-            Attack();
-            lastAttackTime = Time.time; 
-        }
+        if (Time.time - lastAttackTime < attackCooldown)
+            return;
+
+        lastAttackTime = Time.time;
+
+        // Spela animation (kräver Trigger "Attack" i Animator)
+        if (animator != null)
+            animator.SetTrigger("Attack");
+
+        Attack();
     }
 
     void Attack()
     {
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, attackRange, enemyLayer);
+        Collider2D[] hitEnemies =
+            Physics2D.OverlapCircleAll(transform.position, attackRange, enemyLayer);
 
         foreach (Collider2D enemy in hitEnemies)
         {
-            enemy.GetComponent<EnemyHealth>().TakeDamage(damage);
+            var health = enemy.GetComponent<EnemyHealth>();
+            if (health != null)
+                health.TakeDamage(damage);
         }
     }
 
-    private void OnDrawGizmosSelected()
+    void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, attackRange);
